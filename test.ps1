dc3730a6da55bacf55ea02018465cb44d937a17b
sreenathakk <sreenatha.kk@gmail.com>
2018-04-23 12:20:44 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,3 +1,42 @@
+Param(
+    [Parameter(Mandatory = $true)]
+    [String]
+    $VMUsername,
+    [Parameter(Mandatory = $true)]
+    [String]
+    $VMPassword,
+    [Parameter(Mandatory = $true)]
+    [String]
+    $SubscriptionName,
+    [Parameter(Mandatory = $true)]
+    [String]
+    $computername,
+    [Parameter(Mandatory = $true)]
+    [String]
+    $PtrUserName,
+    [Parameter(Mandatory = $true)]
+    [String]
+    $PtrPassword,
+    [Parameter(Mandatory = $true)]
+    [String]
+    $ResourceGroupName,
+    [Parameter(Mandatory = $true)]
+    [String]
+    $DomainName,
+    [Parameter(Mandatory = $true)]
+    [String]
+    $TenantId,
+    [Parameter(Mandatory = $true)]
+    [String]
+    $Location,
+    [Parameter(Mandatory = $true)]
+    [String]
+    $SQLUserName,
+    [Parameter(Mandatory = $true)]
+    [String]
+    $SQLPassword
+)
+
 $vmsecurepass=ConvertTo-SecureString -String $VMPassword -asPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential($VMUsername,$vmsecurepass)
 $so = New-PsSessionOption –SkipCACheck -SkipCNCheck
