df16f94f50d1540ede8c2a6accbd65d8a23a0cd9
sreenathakk <sreenatha.kk@gmail.com>
2018-05-11 19:57:04 +0530

    Add files via upload

---
+++ Script.ps1
@@ -0,0 +1,59 @@
+﻿
+Param(
+    [Parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]
+    [string] $RdbrokerURI,
+
+    [Parameter(Mandatory=$false)]
+    [ValidateNotNullOrEmpty()]
+    [string] $fileURI,
+
+    [Parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]
+    [string] $TenantName,
+
+    [Parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]
+    [string] $AadTenantId,
+
+    [Parameter(Mandatory=$false)]
+    [ValidateNotNullOrEmpty()]
+    [string] $FriendlyName,
+
+    [Parameter(Mandatory=$false)]
+    [ValidateNotNullOrEmpty()]
+    [string] $Description,
+
+    [Parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]
+    [string] $HostPoolName,
+
+    [Parameter(Mandatory=$false)]
+    [ValidateNotNullOrEmpty()]
+    [string] $HostPoolFriendlyName,
+
+    [Parameter(Mandatory=$false)]
+    [ValidateNotNullOrEmpty()]
+    [string] $HostPoolDescription,
+         
+    [Parameter(Mandatory=$True)]
+    [String] $Username,
+    [Parameter(Mandatory=$True)]
+    [string] $Password
+
+
+  
+)
+
+
+Invoke-WebRequest -Uri $fileURI -OutFile "C:\PowershellModules.zip"
+Expand-Archive "C:\PowershellModules.zip" -DestinationPath "C:\"
+Import-Module "C:\PowershellModules\Microsoft.RDInfra.RDPowershell.dll"
+$SecurePass = $Password | ConvertTo-SecureString -asPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential($Username,$SecurePass)
+Set-RdsContext -DeploymentUrl $RdbrokerURI -Credential $Credential
+$newRdsTenant=New-RdsTenant -Name $TenantName -AadTenantId $AadTenantId -FriendlyName $FriendlyName -Description $Description
+$newRDSHostPool=New-RdsHostPool -TenantName $tenName -Name $HostPoolName -Description $HostPoolDescription -FriendlyName $HostPoolFriendlyName
+
+Remove-Item -Path "C:\PowershellModules.zip" -Recurse -force
+Remove-Item -Path "C:\PowershellModules" -Recurse -Force
