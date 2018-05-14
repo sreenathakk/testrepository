1731ce5cd879daea1a2522a7483e50da0f3de370
sreenathakk <sreenatha.kk@gmail.com>
2018-05-14 17:12:39 +0530

    Update Script.ps1

--- Script.ps1
+++ Script.ps1
@@ -53,6 +53,7 @@ $SecurePass = $Password | ConvertTo-SecureString -asPlainText -Force
 $Credential = New-Object System.Management.Automation.PSCredential($Username,$SecurePass)
 Set-RdsContext -DeploymentUrl $RdbrokerURI -Credential $Credential
 $newRdsTenant=New-RdsTenant -Name $TenantName -AadTenantId $AadTenantId -FriendlyName $FriendlyName -Description $Description
+start-sleep -seconds 10
 $newRDSHostPool=New-RdsHostPool -TenantName $tenName -Name $HostPoolName -Description $HostPoolDescription -FriendlyName $HostPoolFriendlyName
 
 Remove-Item -Path "C:\PowershellModules.zip" -Recurse -force
