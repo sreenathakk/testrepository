13df7068d7386d8c1f164c49d97ef0ebcb2204da
viswanadham <sreenatha.kk@gmail.com>
2018-05-21 14:37:35 +0530

    created new file

---
+++ Tenantcreation/Scripts/RemoveRG.ps1
@@ -0,0 +1,27 @@
+﻿Param(
+    [Parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]
+    [string] $AadTenantId,
+    [Parameter(Mandatory=$True)]
+    [String] $Username,
+    [Parameter(Mandatory=$True)]
+    [string] $Password,
+    [Parameter(Mandatory=$True)]
+    [string] $ResourceGroupName
+ 
+)
+
+
+$Securepass=ConvertTo-SecureString -String $Password -AsPlainText -Force
+$Azurecred=New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList($Username, $Securepass)
+Install-Module -Name AzureRM.Profile -AllowClobber -Force
+Install-Module -Name AzureRM.Compute -AllowClobber -Force
+Import-Module -Name AzureRM.Profile
+Import-Module -Name AzureRM.Compute
+
+$login=Login-AzureRmAccount -Credential $Azurecred -TenantId $AadTenantId
+
+$ResourceGroup=Get-AzureRmResourceGroup -Name $ResourceGroupName
+if($ResourceGroup){
+Remove-AzureRmResourceGroup -Name $ResourceGroupName -Force
+}
---
+++ Tenantcreation/Scripts/Script.ps1
@@ -0,0 +1,61 @@
+﻿﻿
+Param(
+    [Parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]
+    [string] $RdbrokerURI,
+
+    [Parameter(Mandatory=$false)]
+    [ValidateNotNullOrEmpty()]
+    [string] $fileURI,
+
+    [Parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]
+    [string] $TenantName,
+
+    [Parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]
+    [string] $AadTenantId,
+
+    [Parameter(Mandatory=$false)]
+    [ValidateNotNullOrEmpty()]
+    [string] $FriendlyName,
+
+    [Parameter(Mandatory=$false)]
+    [ValidateNotNullOrEmpty()]
+    [string] $Description,
+
+    [Parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]
+    [string] $HostPoolName,
+
+    [Parameter(Mandatory=$false)]
+    [ValidateNotNullOrEmpty()]
+    [string] $HostPoolFriendlyName,
+
+    [Parameter(Mandatory=$false)]
+    [ValidateNotNullOrEmpty()]
+    [string] $HostPoolDescription,
+         
+    [Parameter(Mandatory=$True)]
+    [String] $Username,
+    [Parameter(Mandatory=$True)]
+    [string] $Password,
+    [Parameter(Mandatory=$True)]
+    [string] $ResourceGroupName
+ 
+)
+
+
+Invoke-WebRequest -Uri $fileURI -OutFile "C:\PSModules.zip"
+Expand-Archive "C:\PSModules.zip" -DestinationPath "C:\"
+Import-Module "C:\PSModules\Microsoft.RDInfra.RDPowershell.dll"
+$SecurePass = $Password | ConvertTo-SecureString -asPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential($Username,$SecurePass)
+Set-RdsContext -DeploymentUrl $RdbrokerURI -Credential $Credential
+$newRdsTenant=New-RdsTenant -Name $TenantName -AadTenantId $AadTenantId -FriendlyName $FriendlyName -Description $Description
+$newRDSHostPool=New-RdsHostPool -TenantName $TenantName  -Name $HostPoolName -Description $HostPoolDescription -FriendlyName $HostPoolFriendlyName
+
+Remove-Item -Path "C:\PowershellModules.zip" -Recurse -force
+Remove-Item -Path "C:\PowershellModules" -Recurse -Force
+
+.\RemoveRG.ps1 -AadTenantId $AadTenantId -Username $Username -Password $Password -ResourceGroupName $ResourceGroupName
